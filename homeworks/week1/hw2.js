51467e812b6008d1d2975c2cd0487bafd0e3dab5
王立心 <wanglixin@wanglixindeMacBook-Air-2.local>
2018-09-05 23:18:30 +0800

    hw1,2 都各完成一個解法，不過hw2嘗試其他方法中

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -1,3 +1,54 @@
+//用replace
 function capitalize(str) {
-  
+	var newStr = str.replace(str[0],str[0].toUpperCase());
+return newStr;
 }
+console.log(capitalize("hello"))
+
+//test case
+console.log(capitalize("world"))
+console.log(capitalize('1jkle'))
+console.log(capitalize('Nlse'))
+console.log(capitalize(',jakle'))
+
+
+
+//用
+/*-	function 
+	if(strFirst != strFirst.toUpperCase()){
+		strFirst.toUpperCase();
+	}else{
+		strFirst;
+	};
+
+	return strSeperate.join("");
+
+}
+-*/
+//步驟 1.拆開字串到[]; 2.檢視首字，並把她變大寫; 3.回傳new[]; 4.用join把字串串回
+/*-
+function capitalize(str) {
+	var strSeperate = str.split("");
+	var strFirst = strSeperate[0];
+
+	if (strFisrt >= 'a' && strFisrt <= 'z'){
+		strFisrt.toUpperCase();
+	}
+	var newArr = ....
+} 
+-*/
+
+
+//用splice()拼接，解題(還沒寫完)
+/*-function capitalize(str) {
+	var strSeperate = str.split("");
+	//把第一個字切掉
+	var strFirst = strSeperate.splice(0,1);
+	console.log(strSeperate);
+	if(strFirst != strFirst.toUpperCase()){
+		strFirst.toUpperCase();
+	}else{
+		strFirst;
+	};
+	} 
+-*/
