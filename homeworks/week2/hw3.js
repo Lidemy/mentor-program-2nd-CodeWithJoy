9939fcfa00165797f304b7a0965dc789511ff796
王立心 <wanglixin@wanglixindeMacBook-Air-2.local>
2018-09-11 17:24:59 +0800

    執行npm run test的自動改作業程式後，修訂完畢

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,11 +1,15 @@
 function isPrime(n) {
-  for (var i = 2; i < n; i++) {
-    if(n % i === 0){
-      return false;   
-    }else{
-      return true;
-    }
-	} 
+	if(n ===1){
+		return false;
+	}else{
+  		for (var i = 2; i < n; i++) {
+   			 if(n % i === 0){
+      			return false;   
+    		}else{
+     		 	return true;
+    		}
+  		}
+  	} 
 }
   
 //test
